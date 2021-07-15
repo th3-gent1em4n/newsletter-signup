9b41c7bfb558635a830ec18059dabd6c40ace361
Harshit <harshitsinghfzd11@gmail.com>
2021-07-15 14:53:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname +"/signup.html");
+
+});
+
+app.post("/",function(req,res){
+
+    const firstname = req.body.firstName;
+    const secondname = req.body.secondName;
+    const email = req.body.inputEmail;
+    
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_field: {
+                    FNAME : firstname,
+                    LNAME : secondname
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us6.api.mailchimp.com/3.0/lists/e2a90ef793";
+
+    const option = {
+        method: "POST",
+        auth: "th3gent1em4n:63d9f370238e47ba50bc5cc3bc65d742-us6"
+    }
+
+    const request = https.request(url, option, function(response){
+        
+        if(response.statusCode === 200)
+        {
+            res.sendFile(__dirname +"/success.html", function(){
+                console.log("Successfully Added");
+            });
+        }
+        else{
+            res.sendFile(__dirname +"/failure.html", function(){
+                console.log("Uh! Oh! Failed");
+            });
+        }
+        
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    console.log(request.status);
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Your server is running @ port:3000");
+});
+
+
+// 63d9f370238e47ba50bc5cc3bc65d742-us6
+// e2a90ef793
